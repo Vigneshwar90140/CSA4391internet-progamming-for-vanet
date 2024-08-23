34b0322e0a548acc91eab9ff3bd4959ff1951908
Vigneshwar90140 <122530082+Vigneshwar90140@users.noreply.github.com>
2024-08-23 22:21:34 +0530

    Create page-7.js

---
+++ page-7.js
@@ -0,0 +1,5 @@
+function logout() {
+    alert("You have successfully logged out.");
+    // Here you can add logic to redirect to the login page or perform other actions
+    window.location.href = "login.html"; // Example redirect to a login page
+}
